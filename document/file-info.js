98328368a9a487bee10a417e525cc04778151416
Shuery-Shuai <2463253700@qq.com>
2025-04-07 13:58:59 +0800

    🦄 refactor(*): 移动 `script` 文件夹。
    移动 `script` 文件夹至 `document` 文件夹，明晰分类。

--- document/file-info.js
+++ document/file-info.js
@@ -1,4 +1,18 @@
 const redirectFileInfo = {}
 
-const fileList = []
-
+const fileList = [{
+  iconClass: 'icon-directory',
+  name: 'script',
+  type: '文件夹',
+  version: '-',
+  date: '2023/12/15',
+  description: '脚本'
+},
+{
+  iconClass: 'icon-directory',
+  name: 'style',
+  type: '文件夹',
+  version: '-',
+  date: '2023/12/15',
+  description: '样式'
+},]
