0f0f1b357cadf0e2f96d835fe7a3c9c3763ddb66
Shuery-Shuai <2463253700@qq.com>
2025-06-12 00:17:18 +0800

    ✨ feat(document/script): 新增原神启动脚本。

---
+++ document/script/run-genshin.sh
@@ -0,0 +1,378 @@
+#!/usr/bin/env bash
+
+declare -r SCRIPT_NAME="RunGenShin"
+declare -r SCRIPT_DESCRIPTION="一键启动原神并屏蔽指定域名。"
+declare -r SCRIPT_VERSION="1.0.0"
+declare -r SCRIPT_AUTHOR="LetsShareAll"
+
+declare genshin_name="YuanShen"
+declare genshin_launcher="${HOME}/Applications/CrossOver/YuanShen.app/Contents/MacOS/Menu Helper"
+declare temp_block_domain="dispatchcnglobal.yuanshen.com,test.example.com"
+declare local_block_script="${HOME}/Documents/CodeSpace/LetsShareAll/ShareFile/document/script/block-host.sh"
+declare log_level=3 # 0：不输出日志, 1：输出错误日志, 2：输出警告日志, 3：输出信息日志, 4：输出调试日志
+
+declare -r ARG_TIP="请使用 ${0} -h 或 ${0} --help 查看帮助信息。"
+declare -r REMOTE_BLOCK_SCRIPT="https://file.lssa.fun/document/script/block-host.sh"
+declare -r NAME_LENGTH_MIN=3
+declare -r DOMAIN_LENGTH_MIN=3
+
+declare -A STYPE_FORMAT=(
+    [BOLD]="\e[1m"
+    [DIM]="\e[2m"
+    [UNDERLINE]="\e[4m"
+    [BLINK]="\e[5m"
+    [REVERSE]="\e[7m"
+    [HIDDEN]="\e[8m"
+    [RESET_ALL]="\e[0m"
+    [RESET_BOLD]="\e[21m"
+    [RESET_DIM]="\e[22m"
+    [RESET_UNDERLINE]="\e[24m"
+    [RESET_BLINK]="\e[25m"
+    [RESET_REVERSE]="\e[27m"
+    [RESET_HIDDEN]="\e[28m"
+)
+
+declare -A STYLE_FORE_COLORS=(
+    [DEFAULT]="\e[39m"
+    [BLACK]="\e[30m"
+    [RED]="\e[31m"
+    [GREEN]="\e[32m"
+    [YELLOW]="\e[33m"
+    [BLUE]="\e[34m"
+    [MAGENTA]="\e[35m"
+    [CYAN]="\e[36m"
+    [LIGHT_GRAY]="\e[37m"
+    [DARK_GRAY]="\e[90m"
+    [LIGHT_RED]="\e[91m"
+    [LIGHT_GREEN]="\e[92m"
+    [LIGHT_YELLOW]="\e[93m"
+    [LIGHT_BLUE]="\e[94m"
+    [LIGHT_MAGENTA]="\e[95m"
+    [LIGHT_CYAN]="\e[96m"
+    [WHITE]="\e[97m"
+)
+
+declare -A STYLE_BACK_COLORS=(
+    [DEFAULT]="\e[49m"
+    [BLACK]="\e[40m"
+    [RED]="\e[41m"
+    [GREEN]="\e[42m"
+    [YELLOW]="\e[43m"
+    [BLUE]="\e[44m"
+    [MAGENTA]="\e[45m"
+    [CYAN]="\e[46m"
+    [LIGHT_GRAY]="\e[47m"
+    [DARK_GRAY]="\e[100m"
+    [LIGHT_RED]="\e[101m"
+    [LIGHT_GREEN]="\e[102m"
+    [LIGHT_YELLOW]="\e[103m"
+    [LIGHT_BLUE]="\e[104m"
+    [LIGHT_MAGENTA]="\e[105m"
+    [LIGHT_CYAN]="\e[106m"
+    [WHITE]="\e[107m"
+)
+
+declare -A STYLE=(
+    [ERROR]="${STYLE_FORE_COLORS[RED]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMAT[BOLD]}"
+    [WARN]="${STYLE_FORE_COLORS[YELLOW]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMAT[BOLD]}"
+    [INFO]="${STYLE_FORE_COLORS[CYAN]}${STYLE_BACK_COLORS[DEFAULT]}"
+    [NOTE]="${STYLE_FORE_COLORS[LIGHT_MAGENTA]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMAT[UNDERLINE]}"
+    [PROGRESS]="${STYLE_FORE_COLORS[LIGHT_BLUE]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMAT[BLINK]}"
+    [SUCCESS]="${STYLE_FORE_COLORS[GREEN]}${STYLE_BACK_COLORS[DEFAULT]}"
+    [DEBUG]="${STYLE_FORE_COLORS[MAGENTA]}${STYLE_BACK_COLORS[DEFAULT]}"
+    [TIMESTAMP]="${STYLE_FORE_COLORS[LIGHT_GRAY]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMAT[DIM]}"
+    [HIGHLIGHT]="${STYPE_FORMAT[UNDERLINE]}${STYPE_FORMAT[REVERSE]}"
+    [RESET_HIGHLIGHT]="${STYPE_FORMAT[RESET_UNDERLINE]}${STYPE_FORMAT[RESET_REVERSE]}"
+    [RESET]="${STYLE_FORE_COLORS[DEFAULT]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMAT[RESET_ALL]}"
+)
+
+parse_args() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+        -n | --name)
+            if [[ -z "$2" ]]; then
+                log ERROR "未指定原神进程名称！${ARG_TIP}"
+                exit 1
+            fi
+            genshin_name="$2"
+            shift 2
+            ;;
+        -l | --launcher)
+            if [[ -z "$2" ]]; then
+                log ERROR "未指定原神启动文件！${ARG_TIP}"
+                exit 1
+            fi
+            genshin_launcher="$2"
+            shift 2
+            ;;
+        -d | --domain)
+            if [[ -z "$2" ]]; then
+                log ERROR "未指定要屏蔽的域名！${ARG_TIP}"
+                exit 1
+            fi
+            temp_block_domain="$2"
+            shift 2
+            ;;
+        -s | --script)
+            if [[ -z "$2" ]]; then
+                log ERROR "未指定本地屏蔽脚本！${ARG_TIP}"
+                exit 1
+            fi
+            local_block_script="$2"
+            shift 2
+            ;;
+        --log-level)
+            if [[ -z "$2" ]]; then
+                log ERROR "未指定日志级别！${ARG_TIP}"
+                exit 1
+            fi
+            log_level="$2"
+            shift 2
+            ;;
+        -h | --help)
+            show_help
+            exit 0
+            ;;
+        -v | --version)
+            echo "${SCRIPT_NAME} 版本 ${SCRIPT_VERSION}"
+            echo "${SCRIPT_DESCRIPTION}"
+            echo "作者：${SCRIPT_AUTHOR}"
+            exit 0
+            ;;
+        *)
+            echo "未知选项：${1}"
+            show_help
+            exit 1
+            ;;
+        esac
+    done
+}
+
+show_help() {
+    echo "${SCRIPT_NAME} - ${SCRIPT_DESCRIPTION}"
+    echo "版本：${SCRIPT_VERSION}"
+    echo "作者：${SCRIPT_AUTHOR}"
+    echo "用法：${0} [选项]"
+    echo "选项："
+    echo "  -n, --name <name>         设置原神进程名称（默认：${genshin_name}）"
+    echo "  -l, --launcher <launcher> 设置原神启动文件（默认：${genshin_launcher}）"
+    echo "  -d, --domain <domain>     设置要屏蔽的域名（默认：${temp_block_domain}）"
+    echo "  -s, --script <script>     设置本地屏蔽脚本（默认：${local_block_script}）"
+    echo "  --log-level <level>       设置日志级别（默认：${log_level}）"
+    echo "  -h, --help                显示此帮助信息"
+    echo "  -v, --version             显示脚本版本信息"
+}
+
+log() {
+    local level="$1"
+    local message="$2"
+    local timestamp="${STYLE[TIMESTAMP]}【$(date '+%Y-%m-%d %H:%M:%S')】${STYLE[RESET]}"
+    local sentence=""
+    case "$level" in
+    INFO)
+        log_num=3
+        sentence="${STYLE[INFO]}【INFO】${message}${STYLE[RESET]}"
+        ;;
+    NOTE)
+        log_num=2
+        sentence="${STYLE[NOTE]}【NOTE】${message}${STYLE[RESET]}"
+        ;;
+    SUCCESS)
+        log_num=3
+        sentence="${STYLE[SUCCESS]}【SUCCESS】${message}${STYLE[RESET]}"
+        ;;
+    WARN)
+        log_num=2
+        sentence="${STYLE[WARN]}【WARN】${message}${STYLE[RESET]}"
+        ;;
+    ERROR)
+        log_num=1
+        sentence="${STYLE[ERROR]}【ERROR】${message}${STYLE[RESET]}"
+        ;;
+    DEBUG)
+        log_num=4
+        sentence="${STYLE[DEBUG]}【DEBUG】${message}${STYLE[RESET]}"
+        ;;
+    *)
+        log_num=0
+        sentence="【UNKNOWN】${message}"
+        ;;
+    esac
+    if [[ $log_num -gt $log_level ]]; then
+        return
+    fi
+    echo -e "${timestamp}${SCRIPT_NAME}${sentence}"
+}
+
+get_privilege() {
+    log INFO "正获取权限：${STYLE[HIGHLIGHT]}sudo${STYLE[RESET_HIGHLIGHT]}……"
+    local exit_code=0
+    sudo -v 2>/dev/null
+    exit_code=$?
+    if [[ exit_code -ne 0 ]]; then
+        log ERROR "未获取权限！错误代码：${STYLE[HIGHLIGHT]}${exit_code}${STYLE[RESET_HIGHLIGHT]}。"
+        exit 1
+    fi
+    log SUCCESS "已获取权限！"
+}
+
+block_domain() {
+    if [[ -z "$temp_block_domain" ]]; then
+        log ERROR "未设置要屏蔽的域名！"
+        exit 101
+    fi
+    if [[ ${#temp_block_domain} -lt "$DOMAIN_LENGTH_MIN" ]]; then
+        log ERROR "屏蔽域名太短！请检查域名：${STYLE[HIGHLIGHT]}${temp_block_domain}${STYLE[RESET_HIGHLIGHT]}。"
+        log NOTE "域名至少有 ${STYLE[HIGHLIGHT]}${DOMAIN_LENGTH_MIN}${STYLE[RESET_HIGHLIGHT]} 个字符，如：d.c。"
+        exit 102
+    fi
+    log INFO "正屏蔽域名：${STYLE[HIGHLIGHT]}${temp_block_domain}${STYLE[RESET_HIGHLIGHT]}……"
+    log WARN "请在屏蔽域名结束前启动并置顶原神！"
+    local exit_code=0
+    if [[ -n "$local_block_script" ]]; then
+        if [[ ! -f "$local_block_script" ]]; then
+            log ERROR "不存在本地屏蔽脚本文件：${STYLE[HIGHLIGHT]}${local_block_script}${STYLE[RESET_HIGHLIGHT]}！"
+            exit 103
+        fi
+        log INFO "正运行本地脚本：${STYLE[HIGHLIGHT]}${local_block_script}${STYLE[RESET_HIGHLIGHT]}。"
+        sudo bash ${local_block_script} -d ${temp_block_domain} 2>/dev/null
+        exit_code=$?
+        if [[ exit_code -ne 0 ]]; then
+            log ERROR "未运行本地脚本！错误代码：${STYLE[HIGHLIGHT]}${exit_code}${STYLE[RESET_HIGHLIGHT]}。"
+            exit 104
+        fi
+    else
+        log INFO "正运行远程脚本：${STYLE[HIGHLIGHT]}${REMOTE_BLOCK_SCRIPT}${STYLE[RESET_HIGHLIGHT]}。"
+        curl -sS $REMOTE_BLOCK_SCRIPT | sudo bash -s -- -d $temp_block_domain 2>/dev/null
+        exit_code=$?
+        if [[ $exit_code -ne 0 ]]; then
+            log ERROR "未运行远程脚本！错误代码：${STYLE[HIGHLIGHT]}${exit_code}${STYLE[RESET_HIGHLIGHT]}。"
+            exit 105
+        fi
+    fi
+    log SUCCESS "已屏蔽域名！"
+    exit 100
+}
+
+start_genshin() {
+    if [[ -z "$genshin_launcher" ]]; then
+        log ERROR "未设置原神启动文件！"
+        exit 201
+    fi
+    if [[ ! -f "$genshin_launcher" ]]; then
+        log ERROR "不存在原神启动文件：${STYLE[HIGHLIGHT]}${genshin_launcher}${STYLE[RESET_HIGHLIGHT]}！"
+        exit 202
+    fi
+    log INFO "正启动原神：${STYLE[HIGHLIGHT]}${genshin_launcher}${STYLE[RESET_HIGHLIGHT]}……"
+    log NOTE "如果原神未启动，请手动启动原神。"
+    local exit_code=0
+    open "$genshin_launcher" 2>/dev/null
+    exit_code=$?
+    if [[ $exit_code -ne 0 ]]; then
+        log ERROR "未启动原神！错误代码：${STYLE[HIGHLIGHT]}${exit_code}${STYLE[RESET_HIGHLIGHT]}。"
+        exit 203
+    fi
+    log SUCCESS "已启动原神！"
+    exit 200
+}
+
+front_genshin() {
+    if [[ -z "$genshin_name" ]]; then
+        log ERROR "未设置原神进程名称！"
+        exit 301
+    fi
+    if [[ ${#genshin_name} -lt "$NAME_LENGTH_MIN" ]]; then
+        log ERROR "原神进程名称太短！请检查名称：${STYLE[HIGHLIGHT]}${genshin_name}${STYLE[RESET_HIGHLIGHT]}。"
+        log NOTE "名称至少有 ${STYLE[HIGHLIGHT]}${NAME_LENGTH_MIN}${STYLE[RESET_HIGHLIGHT]} 个字符。太短会导致匹配结果过多，若名称本身就短可以尝试添加路径，如：YS 改为 CrossOver/YS。"
+        exit 302
+    fi
+    log INFO "正置顶原神：${STYLE[HIGHLIGHT]}${genshin_name}${STYLE[RESET_HIGHLIGHT]}……"
+    log WARN "如果原神未启动，脚本将等待 10 秒直到原神进程出现！"
+    log NOTE "如果原神未置顶，请手动置顶原神。"
+    local exit_code=0
+    local genshin_pid
+    local seconds=0
+    while [[ -z "$genshin_pid" || $seconds -lt 10 ]]; do
+        sleep 1
+        genshin_pid=$(pgrep -f "$genshin_name")
+        ((seconds++))
+    done
+    log DEBUG "原神进程 ID：${STYLE[HIGHLIGHT]}${genshin_pid}${STYLE[RESET_HIGHLIGHT]}。"
+    osascript -e "tell application \"System Events\"
+      set frontmost of every process whose unix id is ${genshin_pid} to true
+      end tell" 2>/dev/null
+    exit_code=$?
+    if [[ $exit_code -ne 0 ]]; then
+        log ERROR "未置顶原神！错误代码：${STYLE[HIGHLIGHT]}${exit_code}${STYLE[RESET_HIGHLIGHT]}。"
+        exit 303
+    fi
+    log SUCCESS "已置顶原神！"
+    exit 300
+}
+
+exit_script() {
+    local exit_code="$1"
+    log DEBUG "进程退出，退出代码：${STYLE[HIGHLIGHT]}${exit_code}${STYLE[RESET_HIGHLIGHT]}。"
+    case "$exit_code" in
+    0)
+        log INFO "脚本正常退出！"
+        clean_self
+        ;;
+    100) log INFO "网络屏蔽脚本正常退出！" ;;
+    200) log INFO "启动原神正常退出！" ;;
+    203) log WARN "未启动原神！请手动启动原神。" ;;
+    300) log INFO "置顶原神正常退出！" ;;
+    303) log WARN "未置顶原神！请手动置顶原神。" ;;
+    *)
+        log ERROR "脚本异常退出！请检查日志。"
+        terminate_block_script
+        terminate_yuanshen
+        clean_self
+        ;;
+    esac
+    log DEBUG "进程已退出。"
+}
+
+terminate_block_script() {
+    local block_script_pid=$(pgrep -f "$local_block_script")
+    if [[ -n "$block_script_pid" ]]; then
+        log INFO "正终止屏蔽脚本进程：${STYLE[HIGHLIGHT]}${block_script_pid}${STYLE[RESET_HIGHLIGHT]}……"
+        kill "$block_script_pid" 2>/dev/null
+        log SUCCESS "已终止屏蔽脚本进程！"
+    else
+        log INFO "未找到屏蔽脚本进程，无需终止。"
+    fi
+}
+
+terminate_yuanshen() {
+    local genshin_pid=$(pgrep -f "$genshin_launcher")
+    if [[ -n "$genshin_pid" ]]; then
+        log INFO "正终止原神进程：${STYLE[HIGHLIGHT]}${genshin_pid}${STYLE[RESET_HIGHLIGHT]}……"
+        kill "$genshin_pid" 2>/dev/null
+        log SUCCESS "已终止原神进程！"
+    else
+        log INFO "未找到原神进程，无需终止。"
+    fi
+}
+
+clean_self() {
+    log INFO "正清理子进程……"
+    pkill -P "$$"
+    log SUCCESS "已清理子进程！"
+    log INFO "退出脚本！"
+    exit 0
+}
+
+main() {
+    parse_args "$@"
+
+    trap 'exit_script "$?"' EXIT
+
+    get_privilege
+
+    block_domain &
+    sleep 1 && start_genshin &
+    sleep 2 && front_genshin
+}
+
+main "$@"
