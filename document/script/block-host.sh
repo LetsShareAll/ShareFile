5fc47af0a237510d0a94bf4fe77135d2ebeb7829
Shuery-Shuai <2463253700@qq.com>
2025-06-12 00:18:40 +0800

    🦄 refactor(document/script/block-sh.sh): 更改日志格式。

--- document/script/block-host.sh
+++ document/script/block-host.sh
@@ -2,8 +2,8 @@
 # -*- coding：utf-8 -*-
 #==============================================================================
 # 文件名称：block-host.sh
-# 功能描述：临时屏蔽指定域名，自定义时间后自动恢复 (macOS/Linux)
-# 版本信息：1.1.0
+# 功能描述：临时屏蔽指定域名，自定义时间后自动恢复（macOS/Linux）。
+# 版本信息：1.1.1
 # 编写作者：LetsShareAll
 # 创建日期：2025-06-10
 # 最后修改：2025-06-10
@@ -11,62 +11,132 @@
 # 版权信息：本脚本遵循 MIT 许可证，你可以自由使用、修改和分发。
 #==============================================================================
 
+declare -r SCRIPT_NAME="BlockHost"
+declare -r SCRIPT_DESCRIPTION="临时屏蔽指定域名，自定义时间后自动恢复（macOS/Linux）。"
+declare -r SCRIPT_VERSION="1.1.1"
+declare -r SCRIPT_AUTHOR="LetsShareAll"
+
 ### 全局配置 ###
 declare -a BLOCK_DOMAINS=()       # 要屏蔽的域名数组
 declare -i BLOCK_TIME=10          # 默认屏蔽时长（秒）
 declare -i ENABLE_ANIMATION=0     # 启用进度动画（0/1）
 declare -i LOG_LEVEL=3            # 默认日志级别（1:ERROR, 2:WARNING, 3:INFO, 4:DEBUG）
-declare -x SCRIPT_VERSION="1.1.0" # 脚本版本
 
 ### 颜色定义 ###
-declare -A COLORS=(
-    [reset]="\033[0m"
-    [timestamp]="\033[38;5;244m"
-    [header]="\033[38;5;198m"
-    [code]="\033[38;5;183m"
-    [debug]="\033[38;5;111m"
-    [info]="\033[38;5;87m"
-    [success]="\033[38;5;82m"
-    [progress]="\033[38;5;57m"
-    [warning]="\033[38;5;214m"
-    [error]="\033[38;5;196m"
+declare -A STYPE_FORMATS=(
+    [BOLD]="\e[1m"
+    [DIM]="\e[2m"
+    [UNDERLINE]="\e[4m"
+    [BLINK]="\e[5m"
+    [REVERSE]="\e[7m"
+    [HIDDEN]="\e[8m"
+    [RESET_ALL]="\e[0m"
+    [RESET_BOLD]="\e[21m"
+    [RESET_DIM]="\e[22m"
+    [RESET_UNDERLINE]="\e[24m"
+    [RESET_BLINK]="\e[25m"
+    [RESET_REVERSE]="\e[27m"
+    [RESET_HIDDEN]="\e[28m"
+)
+
+declare -A STYLE_FORE_COLORS=(
+    [DEFAULT]="\e[39m"
+    [BLACK]="\e[30m"
+    [RED]="\e[31m"
+    [GREEN]="\e[32m"
+    [YELLOW]="\e[33m"
+    [BLUE]="\e[34m"
+    [MAGENTA]="\e[35m"
+    [CYAN]="\e[36m"
+    [LIGHT_GRAY]="\e[37m"
+    [DARK_GRAY]="\e[90m"
+    [LIGHT_RED]="\e[91m"
+    [LIGHT_GREEN]="\e[92m"
+    [LIGHT_YELLOW]="\e[93m"
+    [LIGHT_BLUE]="\e[94m"
+    [LIGHT_MAGENTA]="\e[95m"
+    [LIGHT_CYAN]="\e[96m"
+    [WHITE]="\e[97m"
+)
+
+declare -A STYLE_BACK_COLORS=(
+    [DEFAULT]="\e[49m"
+    [BLACK]="\e[40m"
+    [RED]="\e[41m"
+    [GREEN]="\e[42m"
+    [YELLOW]="\e[43m"
+    [BLUE]="\e[44m"
+    [MAGENTA]="\e[45m"
+    [CYAN]="\e[46m"
+    [LIGHT_GRAY]="\e[47m"
+    [DARK_GRAY]="\e[100m"
+    [LIGHT_RED]="\e[101m"
+    [LIGHT_GREEN]="\e[102m"
+    [LIGHT_YELLOW]="\e[103m"
+    [LIGHT_BLUE]="\e[104m"
+    [LIGHT_MAGENTA]="\e[105m"
+    [LIGHT_CYAN]="\e[106m"
+    [WHITE]="\e[107m"
+)
+
+declare -A STYLE=(
+    [ERROR]="${STYLE_FORE_COLORS[RED]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMATS[BOLD]}"
+    [WARN]="${STYLE_FORE_COLORS[YELLOW]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMATS[BOLD]}"
+    [INFO]="${STYLE_FORE_COLORS[CYAN]}${STYLE_BACK_COLORS[DEFAULT]}"
+    [NOTE]="${STYLE_FORE_COLORS[LIGHT_MAGENTA]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMATS[UNDERLINE]}"
+    [PROGRESS]="${STYLE_FORE_COLORS[LIGHT_BLUE]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMATS[BLINK]}"
+    [SUCCESS]="${STYLE_FORE_COLORS[GREEN]}${STYLE_BACK_COLORS[DEFAULT]}"
+    [DEBUG]="${STYLE_FORE_COLORS[MAGENTA]}${STYLE_BACK_COLORS[DEFAULT]}"
+    [TIMESTAMP]="${STYLE_FORE_COLORS[LIGHT_GRAY]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMATS[DIM]}"
+    [HIGHLIGHT]="${STYPE_FORMATS[UNDERLINE]}${STYPE_FORMATS[REVERSE]}"
+    [RESET_HIGHLIGHT]="${STYPE_FORMATS[RESET_UNDERLINE]}${STYPE_FORMATS[RESET_REVERSE]}"
+    [RESET]="${STYLE_FORE_COLORS[DEFAULT]}${STYLE_BACK_COLORS[DEFAULT]}${STYPE_FORMATS[RESET_ALL]}"
 )
 
 ### 日志系统 ###
 log() {
     local level="$1"
     local message="$2"
+    local timestamp="${STYLE[TIMESTAMP]}【$(date '+%Y-%m-%d %H:%M:%S')】${STYLE[RESET]}"
     local icon=""
+    local sentence=""
     local log_num=0
 
     case "$level" in
     debug)
         log_num=4
         icon="◇"
+        sentence="${STYLE[DEBUG]}【DEBUG】${icon} ${message}${STYLE[RESET]}"
         ;;
     info)
         log_num=3
         icon="◆"
+        sentence="${STYLE[INFO]}【INFO】${icon} ${message}${STYLE[RESET]}"
         ;;
     success)
         log_num=3
         icon="✔"
+        sentence="${STYLE[SUCCESS]}【SUCCESS】${icon} ${message}${STYLE[RESET]}"
         ;;
     progress)
         log_num=3
         icon="↺"
+        sentence="${STYLE[PROGRESS]}【PROGRESS】${icon} ${message}${STYLE[RESET]}"
         ;;
     warning)
         log_num=2
         icon="⚠"
+        sentence="${STYLE[WARN]}【WARN】${icon} ${message}${STYLE[RESET]}"
         ;;
     error)
         log_num=1
         icon="✖"
+        sentence="${STYLE[ERROR]}【ERROR】${icon} ${message}${STYLE[RESET]}"
         ;;
     *)
         log_num=0
         icon="?"
+        sentence="【UNKNOWN】${icon} ${message}"
         ;;
     esac
 
@@ -74,8 +144,7 @@ log() {
     ((LOG_LEVEL < log_num)) && return
 
     # 输出带颜色的日志
-    echo -e \
-        "${COLORS[timestamp]}[$(date '+%T')]${COLORS[reset]} ${COLORS[$level]}${icon} ${message}${COLORS[reset]}"
+    echo -e "${timestamp}${SCRIPT_NAME}${sentence}"
 }
 
 ### 检查管理员权限 ###
@@ -98,8 +167,10 @@ check_figlet() {
 ### 显示帮助信息 ###
 show_help() {
     cat <<EOF
-用法：sudo ./block-host.sh [选项]
-
+${SCRIPT_NAME} - ${SCRIPT_DESCRIPTION}
+版本：${SCRIPT_VERSION}
+作者：${SCRIPT_AUTHOR}
+用法：${0} [选项]
 选项:
   -d, --domains <域名列表>  *必须*设置要屏蔽的域名（逗号分隔）
   -t, --time <秒数>         设置屏蔽持续时间（默认：10）
@@ -139,7 +210,7 @@ parse_args() {
             exit 0
             ;;
         -v | --version)
-            echo "block-host.sh ${COLORS[code]}v${SCRIPT_VERSION}${COLORS[reset]}"
+            echo "block-host.sh ${STYLE[HIGHLIGHT]}v${SCRIPT_VERSION}${STYLE[RESET_HIGHLIGHT]}"
             exit 0
             ;;
         *)
@@ -167,7 +238,7 @@ create_backup() {
         exit 1
     fi
 
-    log success "已备份 hosts 文件：${COLORS[code]}$(echo $host_backup)${COLORS[success]}！" >&2
+    log success "已备份 hosts 文件：${STYLE[HIGHLIGHT]}$(echo $host_backup)${STYLE[RESET_HIGHLIGHT]}！" >&2
     echo "$host_backup"
 }
 
@@ -251,15 +322,15 @@ show_progress() {
         local -i interval=$((duration * 1000 / steps))
 
         for ((step = steps; step >= 0; step--)); do
-            printf "\r${COLORS[progress]}剩余时间：["
+            printf "\r${STYLE_FORE_COLORS[LIGHT_BLUE]}剩余时间：["
             printf "%0.s█" $(seq 1 $((step)))
             printf "%0.s░" $(seq 1 $((steps - step)))
-            printf "] %5d 秒${COLORS[reset]}" $((step * duration / steps))
-            sleep "$(bc <<< "scale=2; $interval / 1000")"
+            printf "] %5d 秒${STYLE_FORE_COLORS[DEFAULT]}" $((step * duration / steps))
+            sleep "$(bc <<<"scale=2; $interval / 1000")"
         done
     else
         for ((i = duration; i >= 0; i--)); do
-            printf "\r${COLORS[progress]}剩余时间：%d 秒${COLORS[reset]}" "$i"
+            log progress "剩余时间：${STYLE[HIGHLIGHT]}$i${STYLE[RESET_HIGHLIGHT]} 秒……"
             sleep 1
         done
     fi
@@ -271,13 +342,13 @@ show_progress() {
 ### 主函数 ###
 main() {
     # 显示标题
-    printf "\n${COLORS[header]}"
+    printf "\n${STYLE_FORE_COLORS[LIGHT_GREEN]}"
     if check_figlet; then
         figlet -f slant "Block-Host"
     else
         echo "BLOCK-HOST"
     fi
-    printf "${COLORS[reset]}\n"
+    printf "${STYLE_FORE_COLORS[DEFAULT]}\n"
 
     # 解析参数
     parse_args "$@"
@@ -286,10 +357,10 @@ main() {
     check_root
 
     # 显示配置信息
-    log info "屏蔽域名：${COLORS[code]}${BLOCK_DOMAINS[*]}${COLORS[info]}。"
-    log info "屏蔽时长：${COLORS[code]}${BLOCK_TIME}${COLORS[info]} 秒。"
-    log info "进度动画: ${COLORS[code]}$([ $ENABLE_ANIMATION -ne 0 ] && echo "启用" || echo "禁用")${COLORS[info]}。"
-    log debug "调试模式：${COLORS[code]}启用${COLORS[debug]}。"
+    log info "屏蔽域名：${STYLE[HIGHLIGHT]}${BLOCK_DOMAINS[*]}${STYLE[RESET_HIGHLIGHT]}。"
+    log info "屏蔽时长：${STYLE[HIGHLIGHT]}${BLOCK_TIME}${STYLE[RESET_HIGHLIGHT]} 秒。"
+    log info "进度动画: ${STYLE[HIGHLIGHT]}$([ $ENABLE_ANIMATION -ne 0 ] && echo "启用" || echo "禁用")${STYLE[RESET_HIGHLIGHT]}。"
+    log debug "调试模式：${STYLE[HIGHLIGHT]}启用${STYLE[RESET_HIGHLIGHT]}。"
 
     # 创建备份
     local ho s t
