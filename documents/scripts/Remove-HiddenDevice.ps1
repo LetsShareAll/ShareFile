104bb19461711f2bdab0d7c31f98f1a241afd973
Shuery-Shuai <2463253700@qq.com>
2025-11-28 22:05:26 +0800

    merge: 合并 file 分支更改。

--- documents/scripts/Remove-HiddenDevice.ps1
+++
@@ -1,5 +0,0 @@
-$hiddenDevices = Get-PnpDevice -PresentOnly:$false 
-foreach ($device in $hiddenDevices) {
-    Write-Output "Hidden device: $($device.FriendlyName)"
-	&"pnputil" /remove-device $device.InstanceId
-}
--- documents/scripts/Remove-UselessNetDevice.ps1
+++
@@ -1,4 +0,0 @@
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged" /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" /f
